169c21e0775f307232808ff91b8799389f194a0a
anjeongkyun <dkswjdrbs12@naver.com>
2023-04-10 01:06:18 +0900

    feat: health check controller 구현

---
+++ src/main/java/org/github/follower/solution/api/api/controllers/HealthCheckController.java
@@ -0,0 +1,12 @@
+package org.github.follower.solution.api.api.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping
+    private String healthCheck() {
+        return "OK";
+    }
+}
