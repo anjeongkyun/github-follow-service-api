02d2e97f7fc9695df982c9e2d7d9d668e288583a
anjeongkyun <dkswjdrbs12@naver.com>
2023-04-10 01:04:30 +0900

    init project

---
+++ src/main/java/org/github/follower/solution/api/GithubFollowerApiApplication.java
@@ -0,0 +1,13 @@
+package org.github.follower.solution.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubFollowerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubFollowerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/github/follower/solution/api/GithubFollowerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.github.follower.solution.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubFollowerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
